2fb0815d084a0a5c69d0725a921cb928338af95b
ALUNO - SL102 <SL102@ese.edu.br>
2024-04-15 21:41:14 -0300

    projeto

---
+++ lib/views/servico.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Servico extends StatefulWidget {
+  const Servico({ Key? key }) : super(key: key);
+
+  @override
+  _ServicoState createState() => _ServicoState();
+}
+
+class _ServicoState extends State<Servico> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Servico"),),
+    );
+  }
+}
